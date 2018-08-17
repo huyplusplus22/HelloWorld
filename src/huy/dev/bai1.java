8d995b162dc47d3315af81209331bdeae8d20cf4
unknown <huy.sirmaster@gmail.com>
2018-08-17 16:29:25 +0700

    phuongtrinhbachai

--- src/huy/dev/bai1.java
+++ src/huy/dev/bai1.java
@@ -3,5 +3,29 @@ package huy.dev;
 public class bai1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		int a=1;
+		int b=2;
+		int c=3;
+		float x1;
+		float x2;
+		float delta= b*b-4*a*c;
+		if (a==0) {
+			if (b==0) {
+				System.out.println("Phuong trinh vo nghiem");
+			}	else {
+				System.out.println("Phuong trinh co 1 nghiem la: " + (-c/b));
+			}
+		}
+		if (delta > 0) {
+			x1 =(float) (-b+Math.sqrt(delta)/(2*a));
+			x2 =(float) (-b-Math.sqrt(delta)/(2*a));
+			System.out.println("Phuong trinh co nghiem x1 la: " +x1);
+			System.out.println("Phuong trinh co nghiem x2 la: " +x2);
+		} else if (delta==0){
+			x1= (-b/(2*a));
+		System.out.println("Phuong trinh co nghiem kep la: " + "x1 = x2 =" +x1);
+		} else {
+			System.out.println("Phuong trinh vo nghiem");
+		}	
 	}
 }
