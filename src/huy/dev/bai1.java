881cc53164fabe96791ed18d4278fbe6871ef48d
unknown <huy.sirmaster@gmail.com>
2018-08-22 14:42:08 +0700

    Bangcuuchuong

---
+++ src/huy/dev/bai1.java
@@ -0,0 +1,12 @@
+package huy.dev;
+
+public class bai1 {
+	public static void main(String[] args) {
+		for (int i=2;i<=9;i++) {
+		for (int j=1;j<=10;j++) {
+			System.out.print(j  + " x " + i + " = " + i * j + "\t");
+		}	
+		System.out.println(i); 
+		}
+	}
+}
